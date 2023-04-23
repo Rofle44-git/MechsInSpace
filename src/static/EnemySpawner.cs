7027ad6f48f88be9dfea27062f310ed4e536310c
Rofle44-git <lennard.crafter@gmail.com>
2023-04-23 03:39:57 +0200

    Replacing WaveEnemyList with export variables

--- src/static/EnemySpawner.cs
+++ src/static/EnemySpawner.cs
@@ -2,7 +2,7 @@ using Godot;
 
 public partial class EnemySpawner : Node {
     [Export(PropertyHint.NodePathValidTypes, "Node2D")] NodePath Player;
-    [Export] WaveEnemyList Enemies = new WaveEnemyList();
+    //[Export] WaveEnemyList Enemies = new WaveEnemyList();
     [Export] int StartWave = 1;
     [Export] int EndWave = -1;
     int Wave;
@@ -10,20 +10,20 @@ public partial class EnemySpawner : Node {
     public override void _Ready() {
         Wave = StartWave;
 
-        for (int I = 1; I < 11; I++) {
-            GD.Print("--Wave ", I, "--\nSpawns: ", GetSpawnList(I));
-        }
+    //    for (int I = 1; I < 11; I++) {
+    //        GD.Print("--Wave ", I, "--\nSpawns: ", GetSpawnList(I));
+    //    }
     }
 
-    Godot.Collections.Array<Enemy> GetSpawnList(int wave) {
-        Godot.Collections.Array<Enemy> Return = new Godot.Collections.Array<Enemy>{};
-        foreach (WaveEnemy waveEnemy in Enemies.List) {
-            if (waveEnemy.CanSpawn(wave)) {
-                for (int I = 0; I < waveEnemy.GetSpawnCount(wave); I++) {
-                    Return.Add(waveEnemy.TargetEnemy);
-                }
-            }
-        }
-        return Return;
-    }
+    //Godot.Collections.Array<Enemy> GetSpawnList(int wave) {
+    //    Godot.Collections.Array<Enemy> Return = new Godot.Collections.Array<Enemy>{};
+    //    foreach (WaveEnemy waveEnemy in Enemies.List) {
+    //        if (waveEnemy.CanSpawn(wave)) {
+    //            for (int I = 0; I < waveEnemy.GetSpawnCount(wave); I++) {
+    //                Return.Add(waveEnemy.TargetEnemy);
+    //            }
+    //        }
+    //    }
+    //    return Return;
+    //}
 }
