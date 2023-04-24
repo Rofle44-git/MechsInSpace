00a033314d3ac0cbd8fcc2460f2ba7bb9fc40d19
Rofle44-git <lennard.crafter@gmail.com>
2023-04-24 10:21:48 +0200

    Replacing EnemyWaveList with a bunch of Export Variables

--- src/static/EnemySpawner.cs
+++ src/static/EnemySpawner.cs
@@ -2,28 +2,11 @@ using Godot;
 
 public partial class EnemySpawner : Node {
     [Export(PropertyHint.NodePathValidTypes, "Node2D")] NodePath Player;
-    //[Export] WaveEnemyList Enemies = new WaveEnemyList();
     [Export] int StartWave = 1;
     [Export] int EndWave = -1;
     int Wave;
 
     public override void _Ready() {
         Wave = StartWave;
-
-    //    for (int I = 1; I < 11; I++) {
-    //        GD.Print("--Wave ", I, "--\nSpawns: ", GetSpawnList(I));
-    //    }
     }
-
-    //Godot.Collections.Array<Enemy> GetSpawnList(int wave) {
-    //    Godot.Collections.Array<Enemy> Return = new Godot.Collections.Array<Enemy>{};
-    //    foreach (WaveEnemy waveEnemy in Enemies.List) {
-    //        if (waveEnemy.CanSpawn(wave)) {
-    //            for (int I = 0; I < waveEnemy.GetSpawnCount(wave); I++) {
-    //                Return.Add(waveEnemy.TargetEnemy);
-    //            }
-    //        }
-    //    }
-    //    return Return;
-    //}
 }
