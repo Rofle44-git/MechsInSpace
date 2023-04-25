06c4c9ef451d4505f710b87d79631ff836f9e3ff
Rofle44-git <lennard.crafter@gmail.com>
2023-04-25 16:15:14 +0200

    Remove unneeded resources

--- src/static/WaveEnemyList.cs
+++
@@ -1,5 +0,0 @@
-using Godot;
-
-public partial class WaveEnemyList : Resource {
-    [Export] public WaveEnemy[] List;
-}
--- src/static/wave_enemy_widget.gd
+++
@@ -1,6 +0,0 @@
-extends PanelContainer
-
-
-func set_enemy_name(text : String) -> void:
-	@warning_ignore("unsafe_property_access")
-	$HBoxContainer/Label.text = text;
