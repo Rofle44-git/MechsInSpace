f60f4823e51084dd06453db4aab7219b3ea40251
Rofle44-git <lennard.crafter@gmail.com>
2023-04-25 20:50:06 +0200

    Added animated button

--- src/static/main_menu.gd
+++ src/static/main_menu.gd
@@ -1,6 +1,9 @@
-extends Node2D
+extends Control
+
+
+@export var override_debug_menu : bool = false;
 
 
 func _ready() -> void:
-	if OS.has_feature("debug"):
+	if OS.has_feature("debug") && !override_debug_menu:
 		get_tree().change_scene_to_file("res://dev/dev_menu.tscn");
