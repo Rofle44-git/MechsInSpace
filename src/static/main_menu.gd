26a9cc840b6b2b925350484d41cd0c723f9cb5c5
Rofle44-git <lennard.crafter@gmail.com>
2023-04-25 21:25:24 +0200

    Start button takes you to game.tscn now

--- src/static/main_menu.gd
+++ src/static/main_menu.gd
@@ -7,3 +7,12 @@ extends Control
 func _ready() -> void:
 	if OS.has_feature("debug") && !override_debug_menu:
 		get_tree().change_scene_to_file("res://dev/dev_menu.tscn");
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/static/game.tscn");
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_about_pressed() -> void:
+	pass # Replace with function body.
