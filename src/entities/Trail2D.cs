2408a6397651b8e05e12d9c69d81179ce0ed993c
Rofle44-git <lennard.crafter@gmail.com>
2023-04-27 16:22:05 +0200

    Added some points as a preview

--- src/entities/Trail2D.cs
+++ src/entities/Trail2D.cs
@@ -5,6 +5,11 @@ public partial class Trail2D : Line2D {
 	[Export(PropertyHint.Range, "0,1,0.025")] float Lifetime = 0.6f;
 	[Export] bool Enabled = true;
 
+	public override void _Ready() {
+		// Clears points used as a preview
+        ClearPoints();
+	}
+
 	public override void _Process(double delta) {
 		if (Engine.GetFramesDrawn() % Config.FramesPerPoint == 0) {
 			if (GetPointCount()	> (int)(Lifetime/delta/Config.FramesPerPoint)) RemovePoint(-1);
