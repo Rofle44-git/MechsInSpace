baaf5cc46e791878c6acfcdc51a2d0ecef93558c
Rofle44-git <lennard.crafter@gmail.com>
2023-04-27 16:09:33 +0200

    Added Trail2D Node

---
+++ src/entities/Trail2D.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class Trail2D : Line2D {
+	[Export(PropertyHint.Range, "0,1,0.025")] float Lifetime = 0.6f;
+	[Export] bool Enabled = true;
+
+	public override void _Process(double delta) {
+		if (Engine.GetFramesDrawn() % Config.FramesPerPoint == 0) {
+			if (GetPointCount()	> (int)(Lifetime/delta/Config.FramesPerPoint)) RemovePoint(-1);
+			if (Enabled) AddPoint(GlobalPosition);
+		}
+	}
+}
