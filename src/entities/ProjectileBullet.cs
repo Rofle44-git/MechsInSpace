da8383fee789d2270fa6d747c74e7e41287b391c
Rofle44-git <lennard.crafter@gmail.com>
2023-05-19 20:54:57 +0200

    Now deals damage

--- src/entities/ProjectileBullet.cs
+++ src/entities/ProjectileBullet.cs
@@ -3,6 +3,8 @@ using Godot;
 public partial class ProjectileBullet : Bullet {
     [Export(PropertyHint.Range, "0,3000,1")] int Speed = 1750;
     Vector2 RotationVector;
+    KinematicCollision2D Collision;
+    GodotObject Collider;
 
     public override void _Ready() {
         base._Ready();
@@ -11,6 +13,10 @@ public partial class ProjectileBullet : Bullet {
     
     public override void _PhysicsProcess(double delta) {
         Velocity = Speed * RotationVector;
-        MoveAndSlide();
+        Collision = MoveAndCollide(Velocity*(float)delta);
+        if (Collision == null) return;
+        Collider = Collision.GetCollider();
+        if (!(Collider is Enemy)) return;
+        ((Enemy)Collider).Hurt(Damage);
     }
 }
