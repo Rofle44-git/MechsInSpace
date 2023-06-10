b808cc7beaa46d28f6c8c6e61639e8e04d49158e
Rofle44-git <lennard.crafter@gmail.com>
2023-06-10 16:38:44 +0200

    New CoinPickedUp signal, renamed money to coins

--- src/singletons/Globals.cs
+++ src/singletons/Globals.cs
@@ -1,6 +1,9 @@
 using Godot;
 
 public class Global {
+    public delegate void CoinPickedUp();
+
     public static Node2D Player;
     public static int MaxLifetime = 3;
+    public static int Coins = 0;
 }
