c34f8a702ae850749f38466cf7b9a2cb82ed942f
Rofle44-git <lennard.crafter@gmail.com>
2023-06-21 09:23:59 +0200

    im tired

---
+++ src/entities/Coin.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+public partial class Coin : RigidBody2D {
+	[Export] PackedScene DisappearEffect;
+
+	public void GetCollected() {
+		// HOW DOES IT WORK
+		//Global.EmitSignal(SignalName.CoinPickedUp);
+		Node2D effect = DisappearEffect.Instantiate<Node2D>();
+		effect.GlobalPosition = GlobalPosition;
+		AddSibling(effect);
+		QueueFree();
+	}
+
+	private void _on_collect_area_body_entered(Node2D body) {
+		if (body == Global.Player) {
+			# GO GO GO START MOVING TOD A UHHHH PLAYER FROM GLOBAL:PLAYER NOW
+		}
+	}
+}
