69fc6a1ddc1ef9f985ee3acd9e7b0f8d0065d077
Rofle44-git <lennard.crafter@gmail.com>
2023-06-21 17:37:07 +0200

    Im dumb

--- src/static/joke_label.gd
+++ src/static/joke_label.gd
@@ -8,7 +8,7 @@ var random_string : PackedStringArray = [
 	"it's what they would have wanted",
 	"because that's what heros do",
 	"or die",
-	"all 4294967296 of them",
+	"all 2147483648 of them",
 	"\"shoot em up good\",\n];\n\n\nfunc _ready() -> void:\n\ttext = \"...\"+random_string[randi_range(0, random_string.size()-1)];",
 	"yes that's the title. that's all I came up with",
 	"but don't beat em up",
