c10a14ed174a679a12a19b6333c3f1f6c42e3dba
Rofle44-git <lennard.crafter@gmail.com>
2023-06-25 07:02:18 +0200

    Raycast and line using reliable node export now

--- src/entities/InstantBullet.cs
+++ src/entities/InstantBullet.cs
@@ -1,10 +1,11 @@
 using Godot;
 
 public partial class InstantBullet : Bullet {
+    [Export] RayCast2D Ray;
+    [Export] Line2D Line;
+    
     public override void _Ready() {
         base._Ready();
-        RayCast2D Ray = GetNode<RayCast2D>("RayCast2D");
-        Line2D Line = GetNode<Line2D>("Line2D");
 
         Ray.TargetPosition = new Vector2(MaxDistance, 0.0f).Rotated(Rotation);
         Ray.ForceRaycastUpdate();
