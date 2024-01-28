bbd4595271ab6594291e073d703cbf4b7a7642ee
Rofle44-git <lennard.crafter@gmail.com>
2024-01-28 17:32:19 +0100

    Implemented base entity class

---
+++ src/node/2d/entity.gd
@@ -0,0 +1,11 @@
+class_name Entity
+extends RigidBody2D
+## Base class for Entities.
+##
+## This serves as a base class for all objects capable of performing at least
+## one of the following actions:
+## - interacting with the game-world and/or its objects
+## - reacting to incoming interactions from the game-world and/or its objects
+## If an object DOES have scripted behavior, but it can be imitated with a
+## compound of nodes, it is NOT an entity.
+## As this is a base class, inheriting from it is not intended.
