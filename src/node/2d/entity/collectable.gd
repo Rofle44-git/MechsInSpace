a3d825dd82986ec5d109dbd1037ba4c8a6f5b1ba
Rofle44-git <lennard.crafter@gmail.com>
2024-01-28 17:32:35 +0100

    Implemented collectable class

---
+++ src/node/2d/entity/collectable.gd
@@ -0,0 +1,9 @@
+class_name Collectable
+extends Entity
+## Class for collectable objects.
+##
+## A collectible is an object that is hard-coded to only react to players. Its
+## sole purpose is to be collected by a player, and modify at least one of the
+## player's state/properties.
+##
+## Examples: ammo-box, HP-containers, ability, coin, xp-blobs
