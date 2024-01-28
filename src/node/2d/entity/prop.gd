114252cae82f1275b4b4a7bb3191fc22686fa358
Rofle44-git <lennard.crafter@gmail.com>
2024-01-28 17:32:51 +0100

    Implemented Prop class

---
+++ src/node/2d/entity/prop.gd
@@ -0,0 +1,11 @@
+class_name Prop
+extends Entity
+## Class for reactive objects.
+##
+## A prop is an object that isn't actually sentient, but still reacts to
+## events/interactions, or has complex scripted behavior. If an object is
+## capable of freely interacting with other objects, or has some form of
+## free-will/artificial-intelligence, then it is NOT a Prop (see
+## [ConsciousEntity]).
+##
+## Examples: explosive barrel, destructable walls, crate, door
