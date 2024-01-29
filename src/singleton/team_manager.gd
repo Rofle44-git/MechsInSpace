bc9e76f0e989c6a08759c395c8964942dc81de92
Rofle44-git <lennard.crafter@gmail.com>
2024-01-29 14:54:47 +0100

    Added Species and Planets to the team_manager.gd

--- src/singleton/team_manager.gd
+++ src/singleton/team_manager.gd
@@ -1,1 +1,18 @@
 extends Node
+
+
+enum Species { ## All types of species in the base game.
+	HEXAKRION, ##				Sputnik	orange-ish yellow hexagon
+	TRIGONIKOS_AMETHYSTON, ##		Turret	amethyst triagon
+	GALAZOPRASINA_EPTAGONON, ##		Tank		aquamarine heptagon
+	DILITIRIODI_TETRAGONIKIAN, ##		Crawler	green venomous tetragon
+	DEKAGONIKOS_PORTOKALOSKOROSIAN ##	Moth		decagonal orange moth
+}
+
+enum Planets {
+	HEXAKRIA,
+	TRIGONIKOS_AMETHYSTIA,
+	GALAZOPRASINA_EPTAGONA,
+	DILITIRIODI_TETRAGONIKA,
+	DEKAGONIKOS_PORTOKALOSKOROSIA,
+}
